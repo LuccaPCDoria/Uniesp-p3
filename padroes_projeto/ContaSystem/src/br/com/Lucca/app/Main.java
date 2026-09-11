22eacadab17aa013eea10022320b50ac01740135
Lucca Prisco Chicarini Doria <LuccaPrisco@academicos.com>
2026-09-11 20:08:56 -0300

    <chore>:  refatorando a estrutura de pastas

--- padroes_projeto/ContaSystem/src/br/com/Lucca/app/Main.java
+++ padroes_projeto/ContaSystem/src/br/com/Lucca/app/Main.java
@@ -1,7 +1,11 @@
 package br.com.Lucca.app;
 
+import br.com.Lucca.contas.Conta;
+
 public class Main{
     public static void main(String[] args) {
+        Conta conta = new Conta(101, 500.0);
+
 
     }
 }
